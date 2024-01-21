7bae42c82868b25fd30e4b2cee6264533176bd44
Paresh Chawla <pareshchawla.56@gmail.com>
2024-01-22 02:03:10 +0530

    Moved Routing to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Body from "./components/Body";
+import Error from "./components/Error";
+import Browse from "./components/Browse";
+import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+const router = createBrowserRouter([
+  // Import your routes here instead of defining them in Body
+  {
+    path: "/",
+    element: <Body />,
+    errorElement: <Error />, // Assuming Error component is accessible here
+  },
+  {
+    path: "/browse",
+    element: (
+      <ProtectedRoute>
+        <Browse />
+      </ProtectedRoute>
+    ), // Assuming Browse component is accessible here
+    errorElement: <Error />,
+  },
+  {
+    path: "/error",
+    element: <Error />,
+  },
+]);
 
 function App() {
   return (
     <Provider store={appStore}>
-      <Body />
+      <RouterProvider router={router} />
     </Provider>
   );
 }
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./Login";
-import Browse from "./Browse";
-import Error from "./Error";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
+
+
 
 const Body = () => {
-  const appRouter = createBrowserRouter([
-    {
-      path: "/",
-      element: <Login />,
-      errorElement: <Error />,
-    },
-    {
-      path: "/browse",
-      element: <Browse />,
-    },
-  ]);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
+      } else {
+        dispatch(removeUser());
+      }
+    });
+
+    return unsubscribe;
+  }, [dispatch]);
+
+
   return (
     <>
-      <RouterProvider router={appRouter} />
+      <Login />
     </>
   );
 };
