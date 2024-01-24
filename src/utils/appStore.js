e84353d64ad4329355be9394fe01753327c8fbe5
Paresh Chawla <pareshchawla.56@gmail.com>
2024-01-24 12:36:36 +0530

    Add movieslice to store

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userSlice from "./userSlice";
+import userReducer from "./userSlice";
+import moviesReducer from "./moviesSlice";
 
 const appStore = configureStore({
     reducer: {
-        user: userSlice,
+        user: userReducer,
+        movies: moviesReducer,
     }
 })
 
