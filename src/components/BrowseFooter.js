65cff3b2a8eb33f41d3c426d69fbf0dbde7716ef
Paresh Chawla <pareshchawla.56@gmail.com>
2024-01-24 22:21:23 +0530

    Add footer in browse page

---
+++ src/components/BrowseFooter.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const BrowseFooter = () => {
+  return (
+    <div>
+      <div className="flex justify-center bg-[#141414] h-[20vhh] w-[full]">
+      <div className="w-[90%] border-t border-gray-700">
+        <p className="text-sm text-center my-24 text-[#737373]">
+          &copy; Copyright 2024 Paresh Chawla
+        </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BrowseFooter;
