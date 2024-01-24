309abcb6c0943ad3dfeeb7fb7df0e882bdcb8e3d
Paresh Chawla <pareshchawla.56@gmail.com>
2024-01-24 22:20:28 +0530

    Add poster path

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,5 @@ export const API_OPTIONS = {
     Authorization: "Bearer " + process.env.REACT_APP_TMDB_API_READ_ACCESS_TOKEN ,
   }
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w200";
