1c468ac5ad2536648d29610da589bb5bdecb6be4
Paresh Chawla <pareshchawla.56@gmail.com>
2024-01-24 22:20:00 +0530

    Add content

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -3,16 +3,24 @@ import BrowseHeader from "./BrowseHeader";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
+import usePopularMovies from "../hooks/usePopularMovies";
+import useTopRatedMovies from "../hooks/useTopRatedMovies";
+import useUpcomingMovies from "../hooks/useUpcomingMovies";
+import BrowseFooter from "./BrowseFooter";
 
 const Browse = () => {
   useNowPlayingMovies();
+  usePopularMovies();
+  useTopRatedMovies();
+  useUpcomingMovies();
   return (
     <>
-      <div className="flex">
+      <div>
         <BrowseHeader />
+        <MainContainer />
+        <SecondaryContainer />
+        <BrowseFooter />
       </div>
-      <MainContainer />
-      <SecondaryContainer />
     </>
   );
 };
