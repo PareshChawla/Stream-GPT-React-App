060a5a0d8f28fd648207d2bcc55dc880bff83b8f
Paresh Chawla <pareshchawla.56@gmail.com>
2024-01-26 23:15:00 +0530

    Add GptSearch form

---
+++ src/components/GptSearch.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GptSearch = () => {
+  return (
+    <div>
+        <form>
+            <input type="text" />
+            <button>Search</button>
+        </form>
+    </div>
+  )
+}
+
+export default GptSearch
