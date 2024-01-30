e9854c86184fe830bcef189b089becbd809ca90b
Paresh Chawla <pareshchawla.56@gmail.com>
2024-01-31 01:18:20 +0530

    Add genres slice to redux

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import moviesReducer from "./moviesSlice";
 import gptReducer from "./gptSlice";
+import genresReducer from "./genresSlice";
 
 const appStore = configureStore({
     reducer: {
         user: userReducer,
         movies: moviesReducer,
         gpt: gptReducer,
+        genres: genresReducer,
     }
 })
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,4 +7,4 @@ export const API_OPTIONS = {
   }
 };
 
-export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w200";
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/original";
