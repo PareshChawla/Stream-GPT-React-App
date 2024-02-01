90a8a98093dc2ed98c8eee643e16d6a3e62263a2
Paresh Chawla <pareshchawla.56@gmail.com>
2024-02-01 23:52:08 +0530

    Implement shimmer timer

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import VideoTitle from "./VideoTitle";
 import VideoBackground from "./VideoBackground";
+import ShimmerBrowse from "./ShimmerBrowse";
 
 const MainContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const shimmerTimeout = setTimeout(() => {
+      setLoading(false);
+    }, 1500); // Adjust the duration as needed (in milliseconds)
+
+    return () => clearTimeout(shimmerTimeout);
+  }, []);
+
+  if (loading) return <ShimmerBrowse />;
 
   if (!movies) return;
 
@@ -13,7 +25,7 @@ const MainContainer = () => {
   return (
     <>
       <div>
-        <VideoTitle title={title} overview={overview} genreIDS={genre_ids} />
+        <VideoTitle movieID={id} title={title} overview={overview} genreIDS={genre_ids} />
         <VideoBackground movieID={id} backdropPath={backdrop_path} />
       </div>
     </>
